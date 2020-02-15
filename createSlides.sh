b783686e828e2a929412a3b8a7d3c2f2908fa798
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-02-15 15:26:38 -0600

    Script to update slides

---
+++ createSlides.sh
@@ -0,0 +1,4 @@
+jupyter nbconvert index.ipynb --to slides --reveal-prefix=reveal.js \
+--SlidesExporter.reveal_theme=serif \
+--SlidesExporter.reveal_scroll=True \
+--SlidesExporter.reveal_transition=none
