2dfaa1f9bdeba8c7bafd0b7a47446d6e639fabdb
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-02-15 15:40:43 -0600

    Minor changes to slide generator script

--- createSlides.sh
+++ createSlides.sh
@@ -2,3 +2,8 @@ jupyter nbconvert index.ipynb --to slides --reveal-prefix=reveal.js \
 --SlidesExporter.reveal_theme=serif \
 --SlidesExporter.reveal_scroll=True \
 --SlidesExporter.reveal_transition=none
+
+mv index.slides.html index.html
+git add index.html
+git commit -m "Updating slides"
+git push
