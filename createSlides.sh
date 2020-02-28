ba44bf9ff261e11753dcc721810b328234888747
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-02-27 19:22:09 -0600

    Create slides script updated

--- createSlides.sh
+++ createSlides.sh
@@ -1,9 +1,9 @@
-jupyter nbconvert index.ipynb --to slides --reveal-prefix=reveal.js \
+jupyter nbconvert 2.Main.ipynb --to slides --reveal-prefix=reveal.js \
 --SlidesExporter.reveal_theme=serif \
 --SlidesExporter.reveal_scroll=True \
 --SlidesExporter.reveal_transition=none
 
-mv index.slides.html index.html
+mv 2.Main.slides.html index.html
 git add index.html
 git commit -m "Updating slides"
 git push
